9774f0bd60701113de34e2740265e57602b0cfa4
Iain Woodburn <enjkw13@gmail.com>
2016-07-10 21:41:15 -0500

    Create Show Files.bat
    Only lines 1,28,54 are used.
    Starts the file openFiles
    Lots of comments just to make readability difficult :)

---
+++ Show Files.bat
@@ -0,0 +1,81 @@
+@echo off
+#exit
+#start randFils
+#echo off
+#@echooff
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+start openFiles
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+exit#  randFils
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
+#exit
+#start randFils
+#echo off
